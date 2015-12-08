3006c3f188c35986dfad709146e2dabd34345be3
Maria <zkerkeb@e3r4p10.42.fr>
2015-12-08 22:12:27 +0100

    Anivia bouffe zed

--- zak/ft_try.c
+++ zak/ft_try.c
@@ -6,28 +6,40 @@
 /*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 20:41:57 by zkerkeb           #+#    #+#             */
-/*   Updated: 2015/12/08 21:07:07 by zkerkeb          ###   ########.fr       */
+/*   Updated: 2015/12/08 22:09:55 by zkerkeb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <stdio.h>
+//#include <stdio.h>
 
 int ft_try(char *str, int i, int l)
 {
-	while ((str[i] != '\n') && (str[i] == '.' || str[i] == '#'))
-		i++;
-	if (i != 4)
+	int t;
+
+	t = 0;
+	while ((str[i] != '\n' && str[i] != '\0') && (str[i] == '.' || str[i] == '#'))
 	{
-		printf("error");
-		return (1);
+		t++;
+		i++;
+	}
+//	printf("%d",t);
+//	printf("\n");
+	if (t != 4)
+	{
+	//	printf("error");
+		return (0);
 	}
 	l++;
+	i++;
+	if (str[i + 1] == '\0')
+	   return (0);	
 	if (l == 4)
-		i = i + 2;
-	while (str[i] != '\0')
 	{
-		ft_try(str, i, l);		
-	}
-	return (0); 
+		l = 0;
+		i++ ;
+	}	
+	ft_try(str, i, l);
+	
+	return(0);
 }
